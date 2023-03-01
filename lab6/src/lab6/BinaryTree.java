29f6add32d3bb199ee522c9617df112a77a3d0c8
jgore077 <jgore077@gmail.com>
2023-03-01 10:45:35 -0500

    Binary Tree Class

---
+++ lab6/src/lab6/BinaryTree.java
@@ -0,0 +1,23 @@
+package lab6;
+
+import java.io.NotActiveException;
+
+public class BinaryTree<E>{
+	Node root=null;  
+	static class Node<E>{
+		E data;
+		Node right;
+		Node left;
+		public Node(E val){
+			data=val;
+		}
+	{
+		
+	}
+  }
+  public static void main(String[]args){
+
+  }
+}
+
+
